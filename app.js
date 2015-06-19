2e27d1ebce03f8db92ee0b4efd1d721f0832e42f
giorgiocaviglia <giorgio.caviglia@gmail.com>
2015-06-19 11:42:16 +0200

    Many things

--- app.js
+++ app.js
@@ -1,90 +1,83 @@
-var express = require('express');
-var http = require('http');
-var path = require('path');
+var express = require('express')
+var mongoose = require('mongoose')
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('cookie-session');
+var bodyParser = require('body-parser');
+
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var User = require('./models/user');
 
-//==================================================================
-// Define the strategy to be used by PassportJS
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    if (username === "admin" && password === "admin") // stupid example
-      return done(null, {name: "admin"});
+var app = express()
 
-    return done(null, false, { message: 'Incorrect username.' });
+app.set('views', __dirname + '/public/');
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({ keys: ['secretkey1', 'secretkey2', '...']}));
+
+// static
+app.use(express.static(__dirname + '/public'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
+// Configure passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Connect mongoose
+mongoose.connect('mongodb://localhost/gt3', function(err) {
+  if (err) {
+    console.log('Could not connect to mongodb on localhost. Ensure that you have mongodb running on localhost and mongodb accepts connections on standard ports!');
   }
-));
-
-// Serialized and deserialized methods when got from session
-passport.serializeUser(function(user, done) {
-    done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
-    done(null, user);
+// Register routes
+app.use('/', require('./routes'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// Define a middleware function to be used for every secured routes
-var auth = function(req, res, next){
-  if (!req.isAuthenticated()) 
-  	res.send(401);
-  else
-  	next();
-};
-//==================================================================
+// error handlers
 
-// Start express application
-var app = express();
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.cookieParser()); 
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.session({ secret: 'securedsession' }));
-app.use(passport.initialize()); // Add passport initialization
-app.use(passport.session());    // Add passport initialization
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      title: 'error1',
+      message: err.message,
+      error: err
+    });
+  });
 }
 
-//==================================================================
-// routes
-app.get('/', function(req, res){
-  res.render('index', { title: 'Express' });
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    title: 'error1',
+    message: err.message,
+    error: {}
+  });
 });
 
-app.get('/users', auth, function(req, res){
-  res.send([{name: "user1"}, {name: "user2"}]);
-});
-//==================================================================
+app.set('port', process.env.PORT || 4000);
 
-//==================================================================
-// route to test if the user is logged in or not
-app.get('/loggedin', function(req, res) {
-  res.send(req.isAuthenticated() ? req.user : '0');
-});
-
-// route to log in
-app.post('/login', passport.authenticate('local'), function(req, res) {
-  res.send(req.user);
-});
-
-// route to log out
-app.post('/logout', function(req, res){
-  req.logOut();
-  res.send(200);
-});
-//==================================================================
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });

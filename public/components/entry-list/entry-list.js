1af874508834507798478e68af9ed4e2f1c81272
Cody M Leff <codymleff@gmail.com>
2017-05-02 20:31:15 -0700

    Factored entry list out of Search and into a new directive.

---
+++ public/components/entry-list/entry-list.js
@@ -0,0 +1,95 @@
+app.directive('entryList', function(listService) {
+  
+  return {
+    restrict: 'E',
+    scope: { entries: '=', export: '&' },
+    templateUrl: 'components/entry-list',
+    link: function(scope) {
+
+      if (scope.entries && scope.entries.length) calculateFirstTravelOrders(scope.entries);
+
+      //  initialize view model
+      var viewModel = {
+          newListName: ''
+      };
+
+      //  expose view model to scope
+      scope.viewModel = viewModel;
+
+      //  expose shared list model to scope
+      scope.sharedListModel = listService.sharedListModel
+
+      //  selection/delection commands
+      scope.selectAllEntries = function() {
+        for (var i = 0; i < scope.entries.length; i++) {
+          scope.entries[i].selected = true;
+        }
+      };
+
+      scope.deselectAllEntries = function() {
+        for (var i = 0; i < scope.entries.length; i++) {
+          scope.entries[i].selected = false;
+        }
+      };
+
+      scope.addSelectedEntriesToList = function(list) {
+        for(var i = 0; i < scope.entries.length; i++) {
+          var entry = scope.entries[i];
+          if (entry.selected) {
+            entry.addedToList = entry.alreadyInList = false;
+            listService.addToList(list, entry, (function(result) {
+              if (result.addedToList) {
+                this.entry.addedToList = true;
+              } else if (result.alreadyInList) {
+                this.entry.alreadyInList = true;
+              }
+            }).bind({ entry: entry }));
+          }
+        }
+      };
+
+      scope.addSelectedEntriesToNewList = function() {
+        listService.newList(viewModel.newListName, function(list) {
+          viewModel.newListName = '';
+          console.log('list created: ' + list.name);
+          scope.addSelectedEntriesToList(list);
+        });
+      };
+
+      //  support for sorting entries
+      var sortModel = {
+        activeSortableDimensions: [
+          { label : 'Fullname', sorting : 'fullName' },
+          { label : 'Birth date', sorting : 'dates[0].birthDate' },
+          { label : 'Birth place', sorting : 'places[0].birthPlace' },
+          { label : 'Date of first travel', sorting : 'firstTravelUTC' },
+        ],
+        dimension: 'index',
+        reverseSorting: false
+      };
+
+      scope.sortModel = sortModel;
+
+      function calculateFirstTravelOrders(entries) {
+        for (var i = 0; i < entries.length; i++) {
+
+          var entry = entries[i];
+          if (entry.travels) {
+
+            for (var j = 0; j < entry.travels.length; j++) {
+
+              var travel = entry.travels[j];
+              if (travel.travelStartYear) {
+
+                entry.firstTravelUTC = Date.UTC(travel.travelStartYear, travel.travelStartMonth, travel.travelStartDay);
+                break;
+
+              }
+            }
+          }
+        }
+      }
+
+    },
+  };
+});
--- public/search/search.js
+++ public/search/search.js
@@ -1,7 +1,7 @@
 /**********************************************************************
  * Entries controller
  **********************************************************************/
-app.controller('SearchCtrl', function($scope, $http, $location, $stateParams, listService, entryHighlightingService) {
+app.controller('SearchCtrl', function($scope, $http, $location, $stateParams, entryHighlightingService) {
 
   $scope.query = {};
   $scope.untouched = true;
@@ -33,7 +33,6 @@ app.controller('SearchCtrl', function($scope, $http, $location, $stateParams, li
       entryHighlightingService.saveQuery(res.request)
       $scope.searching = false;
       $scope.entries = res.entries;
-      calculateFirstTravelOrders(res.entries);
       if (res.entries.length) $scope.noResults = false;
       else $scope.noResults = true;
 
@@ -175,112 +174,27 @@ app.controller('SearchCtrl', function($scope, $http, $location, $stateParams, li
 
   $('.tooltip').remove();
 
-  //  initialize view model
-  var viewModel = {
-      newListName: ''
-  };
-
-  //  expose view model to scope
-  $scope.viewModel = viewModel;
-
-  //  expose shared list model to scope
-  $scope.sharedListModel = listService.sharedListModel
-
-  //  selection/delection commands
-  $scope.selectAllEntries = function() {
-    for (var i = 0; i < $scope.entries.length; i++) {
-      $scope.entries[i].selected = true;
-    }
-  };
-
-  $scope.deselectAllEntries = function() {
-    for (var i = 0; i < $scope.entries.length; i++) {
-      $scope.entries[i].selected = false;
-    }
-  };
-
-  $scope.addSelectedEntriesToList = function(list) {
-    for(var i = 0; i < $scope.entries.length; i++) {
-      var entry = $scope.entries[i];
-      if (entry.selected) {
-        entry.addedToList = entry.alreadyInList = false;
-        listService.addToList(list, entry, (function(result) {
-          if (result.addedToList) {
-            this.entry.addedToList = true;
-          } else if (result.alreadyInList) {
-            this.entry.alreadyInList = true;
-          }
-        }).bind({ entry: entry }));
-      }
-    }
-  };
-
-  $scope.addSelectedEntriesToNewList = function() {
-    listService.newList(viewModel.newListName, function(list) {
-      viewModel.newListName = '';
-      console.log('list created: ' + list.name);
-      $scope.addSelectedEntriesToList(list);
-    });
-  };
-
-  //  support for sorting entries
-  var sortModel = {
-    activeSortableDimensions: [
-      { label : 'Fullname', sorting : 'fullName' },
-      { label : 'Birth date', sorting : 'dates[0].birthDate' },
-      { label : 'Birth place', sorting : 'places[0].birthPlace' },
-      { label : 'Date of first travel', sorting : 'firstTravelUTC' },
-    ],
-    dimension: 'index',
-    reverseSorting: false
-  };
-
-  $scope.sortModel = sortModel;
-
-  function calculateFirstTravelOrders(entries) {
-    for (var i = 0; i < entries.length; i++) {
-
-      var entry = entries[i];
-      if (entry.travels) {
-
-        for (var j = 0; j < entry.travels.length; j++) {
-
-          var travel = entry.travels[j];
-          if (travel.travelStartYear) {
-
-            entry.firstTravelUTC = Date.UTC(travel.travelStartYear, travel.travelStartMonth, travel.travelStartDay);
-            break;
-
-          }
-        }
-      }
-    }
-  }
-
   //  export function copied from explore.js
-  $scope.export = function(field, value){
+  $scope.export = function() {
 
-    var $btn = $('#export-button').button('loading')
+    var $btn = $('#export-button').button('loading');
 
-    $http.post('/api/entries/export/', {  query: $scope.query } )
-      .success(function (res){
+    $http.post('/api/entries/export/', { query: $scope.query } )
+    .success(function(res) {
 
-        var entries = d3.tsv.format(res.result.entries);
-        var activities = d3.tsv.format(res.result.activities);
-        var travels = d3.tsv.format(res.result.travels);
+      var entries = d3.tsv.format(res.result.entries);
+      var activities = d3.tsv.format(res.result.activities);
+      var travels = d3.tsv.format(res.result.travels);
 
-        var zip = new JSZip();
-        zip.file("Entries.tsv", entries);
-        zip.file("Activities.tsv", activities);
-        zip.file("Travels.tsv", travels);
-        var content = zip.generate({type:"blob"});
-        saveAs(content, "Grand Tour Explorer - Export.zip");
-        $btn.button('reset')
+      var zip = new JSZip();
+      zip.file("Entries.tsv", entries);
+      zip.file("Activities.tsv", activities);
+      zip.file("Travels.tsv", travels);
+      var content = zip.generate({ type: "blob" });
+      saveAs(content, "Grand Tour Explorer - Export.zip");
+      $btn.button('reset');
 
-      })
-
-
-
+    });
   }
 
   //  download counts

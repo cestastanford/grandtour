3fe5579d88b112a9686772020b85fd47c2497cd7
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-02-26 10:17:34 -0800

    feat: refactor visualization methods to allow grouping

--- public/visualization.component.ts
+++ public/visualization.component.ts
@@ -33,43 +33,69 @@ export class VisualizationComponent {
     radius = 10;
 
     constructor(private http: HttpClient) {
-        var div = d3.select("body").append("div")	
-        .attr("class", "tool_tip")				
-        .style("opacity", 0);
         this.http.post('/api/entries/search', { query: {} }).toPromise().then((e: any) => {
             let entries = e.entries;
-            // console.log(entries);
-            let i = 0;
-            let x = 0;
-            let y = 10;
-            let width = d3.select("svg")[0][0].clientWidth;
-            for (let i in entries) {
-                let entry = entries[i];
-                x += Math.random() * 10 + 10;
-                if (x > width) {
-                    x = 0;
-                    y += 10;
-                }
-                // todo: hover boundary of 2px
-                d3.select('svg').append('circle').attr('cx', x).attr('cy', y).attr('r', 3).attr('fill', 'grey')
-                    // we define "mouseover" handler, here we change tooltip
-                    // visibility to "visible" and add appropriate test
-
-                    .on("mouseover", function(d) {
-                        div.transition()		
-                            .duration(200)		
-                            .style("opacity", .9);		
-                        div	.html(entry.fullName)	
-                            .style("left", (d3.event.pageX) + "px")		
-                            .style("top", (d3.event.pageY - 28) + "px");	
-                        })					
-                    .on("mouseout", function(d) {		
-                        div.transition()		
-                            .duration(500)		
-                            .style("opacity", 0);	
-                    });
-            }
+            let {x, y} = this.drawDots(entries);
+            console.log(x, y);
+            this.groupByType();
         })
+    }
+    clear() {
+        d3.selectAll("svg > *").remove();
+    }
+
+    drawDots(entries, { x = 0, y = 10, random = false } = {}) {
+        let div = d3.select("body").append("div")
+            .attr("class", "tool_tip")
+            .style("opacity", 0);
+        let i = 0;
+        let width = d3.select("svg")[0][0].clientWidth;
+        for (let i in entries) {
+            let entry = entries[i];
+            if (random) {
+                x += Math.random() * 10 + 10;
+            }
+            else {
+                x += 10;
+            }
+            if (x > width) {
+                x = 0;
+                y += 10;
+            }
+            // todo: hover boundary of 2px
+            d3.select('svg').append('circle').attr('cx', x).attr('cy', y).attr('r', 3).attr('fill', 'grey')
+                // we define "mouseover" handler, here we change tooltip
+                // visibility to "visible" and add appropriate test
+
+                .on("mouseover", function (d) {
+                    div.transition()
+                        .duration(200)
+                        .style("opacity", .9);
+                    div.html(entry.fullName)
+                        .style("left", (d3.event.pageX) + "px")
+                        .style("top", (d3.event.pageY - 28) + "px");
+                })
+                .on("mouseout", function (d) {
+                    div.transition()
+                        .duration(500)
+                        .style("opacity", 0);
+                });
+        }
+        return {x, y};
+    }
+
+    async groupByType() {
+        this.clear();
+        let entries_list= await Promise.all([
+            this.http.post('/api/entries/search', { query: {type: "Man"} }).toPromise(),
+            this.http.post('/api/entries/search', { query: {type: "Woman"} }).toPromise()
+        ]);
+        let x, y = 0;
+        for (let response of entries_list) {
+            let result = this.drawDots((response as {entries: any[], request: any}).entries, {x, y, random: false});
+            x = 0;
+            y = result.y + 50;
+        }
     }
 
     ngAfterContentInit() {

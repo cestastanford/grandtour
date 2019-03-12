9fdba362b75614f745de0ecd554a8cc0cff82a5e
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-03-11 21:23:56 -0700

    fix: fix coordinates for one particular place

--- db/fix-coordinates.js
+++ db/fix-coordinates.js
@@ -1,17 +1,19 @@
+/*
+node -r dotenv/config db/fix-coordinates.js > output.log
+*/
 var MongoClient = require('mongodb').MongoClient
 const csv=require('csvtojson');
-// var url = 'mongodb://heroku_4rpt6s2b:20uihpahtlbtngigubmdbgce4m@ds157723.mlab.com:57723/heroku_4rpt6s2b';
-var url = 'mongodb://heroku_c4kbv2zc:li372oaj29alsklj0045gmfn4c@ds213975-a0.mlab.com:13975,ds213975-a1.mlab.com:13975/heroku_c4kbv2zc?replicaSet=rs-ds213975';
-const csvFilePath = "missing_coordinates.csv";
+var url = process.env.MONGODB_URI_DEV;
+const csvFilePath = "db/missing_Albano.csv";
 const {find} = require("lodash");
 
 async function main() {
-
     let db = (await MongoClient.connect(url));
     let allTravelData = await csv().fromFile(csvFilePath);
     let entries = db.collection('entries');
     // console.log(await entries.find({_revisionIndex: 15}).count());
-    let results = await entries.find({}).toArray();
+    let filter = {travels: {$elemMatch: {place: "Albano"}}};
+    let results = await entries.find(filter).toArray();
     for (let item of results) {
         let travels = item.travels;
         // console.log(item._id, item.index);
@@ -25,13 +27,15 @@ async function main() {
                 console.log("=====No travel data found for " + travel.place);
                 continue;
             }
-            // console.log("\t", travel.place, travel.latitude, actualTravelData["FINAL LAT"], travel.longitude, actualTravelData["FINAL LON"], actualTravelData["Italy"]);
+            console.log("\t", travel.place, travel.latitude, actualTravelData["FINAL LAT"], travel.longitude, actualTravelData["FINAL LON"], actualTravelData["Italy"]);
 
             travel.latitude = actualTravelData["FINAL LAT"];
             travel.longitude = actualTravelData["FINAL LON"];
             travel.italy = actualTravelData["Italy"] === "Y";
         }
-        entries.updateOne({_id: item._id}, {$set: {travels: travels}});
+        console.log("updating", item._id);
+        await entries.updateOne({_id: item._id}, {$set: {travels: travels}});
+        console.log("DONE");
         // break;
     }
 }

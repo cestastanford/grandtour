bb5269a5139a1ee2fd3f2c9a4d0917eaaaa47c90
Cody M Leff <mail@codymleff.com>
2019-11-06 21:07:39 -0800

    Added hashchange listener that notifies embedding frame.

--- public/app.module.ajs.ts
+++ public/app.module.ajs.ts
@@ -39,6 +39,7 @@ require('bootstrap');
 /**********************************************************************
  * Angular Application
  **********************************************************************/
+const BOOK_ORIGIN = 'http://localhost:12345';
 const MODULE_NAME = 'app';
  const app = angular.module(MODULE_NAME, [
   ngAnimate,
@@ -105,7 +106,16 @@ const MODULE_NAME = 'app';
 
   }]
 )
-
+.run(function() {
+  if (window.parent !== window) {
+    window.addEventListener('hashchange', function(event) {
+      window.parent.postMessage(
+        { navigatedPath: location.pathname + location.hash },
+        BOOK_ORIGIN,
+      );
+    });
+  }
+})
 .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
   //  Retrieves the user object

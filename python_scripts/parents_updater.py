6be6593bce24e3d36ee12214b505135f29c8ba58
Ashwin Ramaswami <aramaswamis@gmail.com>
2023-05-04 00:23:43 +0000

    Use bulk updates for parents_updater.py

--- python_scripts/parents_updater.py
+++ python_scripts/parents_updater.py
@@ -4,13 +4,27 @@ Splits parents field into an array.
 import json
 import os
 import pymongo
-from pymongo import MongoClient
+from pymongo import MongoClient, UpdateOne
+from pymongo.errors import BulkWriteError
+from tqdm import tqdm
 
 uri = os.getenv("MONGODB_URI")
 client = MongoClient(uri)
 database_name = pymongo.uri_parser.parse_uri(uri)['database']
 db = client[database_name]
 
-for entry in db.entries.find({}):
+bulk_updates = []
+
+for entry in tqdm(db.entries.find({})):
+    if type(entry["parents"]) is list:
+        continue
     new_parents = [entry["parents"]] if entry["parents"] else []
-    db.entries.update_one({"_id": entry['_id']}, {"$set": {"parents": new_parents }})
+    bulk_updates.append(UpdateOne(
+        {"_id": entry['_id']},
+        {"$set": {"parents": new_parents } }
+    ))
+
+try:
+    db.entries.bulk_write(bulk_updates, ordered=False)
+except BulkWriteError as bwe:
+    print(bwe.details)

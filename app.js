498b18427e0d00aff52cdcea9d3720c6cc3f3113
Ashwin Ramaswami <aramaswamis@gmail.com>
2024-05-18 02:53:50 +0000

    Force HTTPS

--- app.js
+++ app.js
@@ -50,6 +50,17 @@ app.use(session({ keys: [
     process.env['SECRET_KEY_3']
 ]}))
 
+// Force HTTPS
+app.use((req, res, next) => {
+    if (process.env.NODE_ENV === 'production') {
+        if (req.headers['x-forwarded-proto'] !== 'https')
+            return res.redirect('https://' + req.headers.host + req.url);
+        else
+            return next();
+    } else
+        return next();
+});
+
 /*
 *   Sets up Passport authentication.
 */
